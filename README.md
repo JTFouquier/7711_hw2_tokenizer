67666c62046f09c963451b4019c68dde3917ee29
JTFouquier <jennietf@gmail.com>
2018-09-08 22:29:43 -0600

    add txt file, still WIP

--- README.md
+++ README.md
@@ -1,15 +1,14 @@
 
 
-
-
 # 7711 Homework 2 Type/Token Ratios
-
 
 Use Python3 to run hw2.py:
 
 If Python3 is your default interpreter, run `python hw2.py` or run
 `python3 hw2.py`
 
+If you have any questions about running this program, please contact
+jennifer dot harper @ucdenver.edu or ping me on the CPBS Slack channel.
 
 ## Original Directions
 
--- hw2.py
+++ hw2.py
@@ -18,12 +18,24 @@ neurology, psychiatry, the social sciences, and the humanities. How
 could a researcher’s definitions of “type" and “token" affect whether
 or not they have a positive result to report?
 
+https://www.researchgate.net/post/What_is_differance_between_String_and_token_
+in_Natural_Language_Processing_techniques
+
+A simplified definition of a token in NLP is as follows: A token is a string
+of contiguous characters between two spaces, or between a space and
+punctuation marks. A token can also be an integer, real, or a number with a
+colon (time, for example: 2:00). All other symbols are tokens themselves
+xcept apostrophes and quotation marks in a word (with no space), which in many
+cases symbolize acronyms or citations. A token can present a single word or a
+group of words (in morphologically rich languages such as Hebrew) as the
+following token "ולאחי" (VeLeAhi) that includes 4 words "And to my brother".
+
 """
 import re
 from collections import Counter
 
 # TODO Get text information from Harrison
-with open('text.txt', 'r') as fin:
+with open('11532192.txt', 'r') as fin:
     my_text = fin.read()
 
 # re to remove special chars and punctuation
@@ -39,6 +51,73 @@ type_count = len(text_histogram)
 print('\nType count:\n', type_count)
 
 
+import re
+
+### Attempt two
+
+with open('11532192.txt', 'r') as fin:
+    my_text = fin.read()
 
 
+new_text = re.sub('\n', '.', my_text)
+new_text = my_text.split()
 
+cleaned_text_list = []
+
+# TODO lowercase conflicts
+
+count_incomplete = 0
+count_complete = 0
+
+for i in new_text:
+    count_complete += 1
+
+
+    # if it's *only* a special char, don't keep it.
+
+    # if it's a float, keep it as is. remove trailing '.'
+    if re.search('\.', i):
+        try:
+            float(i)
+            i = i.strip('\.')
+            cleaned_text_list.append(i.lower())
+            continue
+
+        except:
+            pass
+
+    if re.match(r'^\w+$', i):
+        cleaned_text_list.append(i.lower())
+        continue
+
+
+    # after handling numbers, it's safe to clean a bit more
+
+    for j in [':', '\.', '\)', '\(', ',', ';', '\]', '\[']:
+
+        i = re.sub(j, '', i)
+
+    i = i.strip('"')
+    i = i.strip("'")
+    i = i.strip()
+
+    if len(i) == 0:
+        continue
+
+    if len(i) == 1:
+        cleaned_text_list.append(i.lower())
+        continue
+
+    cleaned_text_list.append(i.lower())
+    count_incomplete += 1
+
+print(count_complete)
+print(count_incomplete)
+
+
+print(len(cleaned_text_list))
+print(cleaned_text_list)
+
+c = Counter(cleaned_text_list)
+print(c)
+print(len(c))
